0d1c1bf7e6b220c34b4250a5fb12b35beb65dae9
coulof <florian.coulombel@dell.com>
2022-03-04 09:00:29 -0500

    Bump version & new reports

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@ NS=${NS:-default}
 
 kubectl get customresourcedefinitions | grep k8s.elastic.co --quiet
 if [ $? -ne 0 ];
-  then echo "installing elastic CRD"; kubectl apply -f https://download.elastic.co/downloads/eck/1.1.1/all-in-one.yaml
+  then echo "installing elastic CRD"; kubectl apply -f https://download.elastic.co/downloads/eck/1.5.0/all-in-one.yaml
 fi
 
 #helm template demo --namespace $NS k8s-events-reporting/
-helm install --name-template demo --namespace $NS ./k8s-events-reporting
+helm install k8s-events-reporting --namespace $NS ./k8s-events-reporting
